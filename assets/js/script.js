4f6975ee86f954952a75c8df454e0aacdd0b3551
Moinul Moin <moinulmoin101@gmail.com>
2021-07-07 16:01:30 +0600

    added mobile responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const menuButton = document.querySelector('#menu-icon')
 const sideBar = document.querySelector('#sidebar')
 const main = document.querySelector('#main')
+const MainContent = document.querySelector('#main-content')
+const allIconDetails = document.querySelectorAll('.icon-details')
 
 menuButton.addEventListener('click', ()=>{
-    sideBar.classList.toggle('hide')
-    main.classList.toggle('full-width')
+    // sideBar.classList.toggle('hide')
+    allIconDetails.forEach(iconDetails => iconDetails.classList.toggle('hide'))
+    MainContent.classList.toggle('change')
 })
